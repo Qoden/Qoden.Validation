6e6c2a154b58017fa7b7fafc2f3bcfed2211fa80
Andrey Verbin <info@studiomobile.ru>
2017-03-28 18:52:51 +0300

    Add argument and operation validators test

---
+++ Qoden.Validation.Test/src/SpecalValidatorsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XAssert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Qoden.Validation.Test
+{
+	[TestClass]
+	public class SpecalValidatorsTest
+	{
+		[TestMethod]
+		public void AssertContainsError()
+		{
+			var ex = XAssert.ThrowsException<ArgumentException>(
+				() => Assert.Argument("", "key").NotEmpty());
+			XAssert.IsNotNull(ex.GetQodenError());
+			XAssert.IsInstanceOfType(ex.GetQodenError(), typeof(Error));
+		}
+
+		[TestMethod]
+		public void InvalidOperationContainsError()
+		{
+			var ex = XAssert.ThrowsException<InvalidOperationException>(
+				() => Assert.State("", "key").NotEmpty());			
+			XAssert.IsNotNull(ex.GetQodenError());
+			XAssert.IsInstanceOfType(ex.GetQodenError(), typeof(Error));
+		}
+	}
+}
