99013f286e81ea2e65405d0270266489d2778576
Andrey Verbin <info@studiomobile.ru>
2017-06-11 00:47:18 +0300

    Migrate to netstandard 1.4 and use stable Qoden dependencies. Add more tests

---
+++ Qoden.Validation.Test/src/CheckDataMemberTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XAssert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Qoden.Validation.Test
+{
+    [TestClass]
+    public class CheckDataMemberTest : ValidationsTestBase
+    {
+        public class Dto
+        {
+            [DataMember]
+            public string Title { get; set; }
+            [DataMember(Name = "description")]
+            public string Description { get; set; }
+            public object NonMember { get; internal set; }
+        }
+
+        [TestMethod]
+        public void CheckDataMemberWithoutName()
+        {
+            var dto = new Dto();
+            Validator.CheckDataMember(dto, x => x.Title).NotNull();
+            var error = Validator.ErrorForKey("Title");
+            XAssert.IsNotNull(error);
+        }
+
+        [TestMethod]
+        public void CheckDataMemberWithName()
+        {
+            var dto = new Dto();
+            Validator.CheckDataMember(dto, x => x.Description).NotNull();
+            var error = Validator.ErrorForKey("description");
+            XAssert.IsNotNull(error);
+        }
+
+        [TestMethod]
+        public void CheckNonDataMember()
+        {
+            var dto = new Dto();
+            XAssert.ThrowsException<ArgumentException>(() =>
+            {
+                Validator.CheckDataMember(dto, x => x.NonMember).NotNull();
+            });
+        }
+    }
+}
--- Qoden.Validation/src/DataMemberPropertyValidatorExtension.cs
+++ Qoden.Validation/src/DataMemberPropertyValidatorExtension.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Runtime.Serialization;
 using Qoden.Reflection;
 
-namespace Qoden.Validation.AspNetCore
+namespace Qoden.Validation
 {
     public static class DataMemberPropertyValidatorExtension
     {
@@ -17,11 +17,16 @@ namespace Qoden.Validation.AspNetCore
             var dataProperty = PropertyName(property);
             var dataMember = dataProperty.GetCustomAttribute<DataMemberAttribute>();
 
-            Assert.State(dataMember, "property")
+            Assert.Argument(dataMember, "property")
                 .NotNull("Property {PropertyName} does not have DataMember attribute",
                     e => { e.Add("PropertyName", dataProperty.Name); });
+            var name = dataMember.Name;
+            if (string.IsNullOrEmpty(name)) 
+            {
+                name = dataProperty.Name;
+            }
             var value = (T) Inspection.GetValue(dto, dataProperty);
-            return validator.CheckValue(value, dataMember.Name, onError);
+            return validator.CheckValue(value, name, onError);
         }
 
         public static Check<T> CheckColumn<T, TDto>(this IValidator validator, TDto dto,
@@ -33,11 +38,16 @@ namespace Qoden.Validation.AspNetCore
 
             var dataMember = dataProperty.GetCustomAttribute<ColumnAttribute>();
 
-            Assert.State(dataMember, "property")
-                .NotNull("Property {PropertyName} does not have ColumnMember attribute",
+            Assert.Argument(dataMember, "property")
+                .NotNull("Property {PropertyName} does not have Column attribute",
                     e => { e.Add("PropertyName", propertyName); });
+            var name = dataMember.Name;
+            if (string.IsNullOrEmpty(name))
+            {
+                name = dataProperty.Name;
+            }
             var value = (T) Inspection.GetValue(dto, dataProperty);
-            return validator.CheckValue(value, dataMember.Name, onError);
+            return validator.CheckValue(value, name, onError);
         }
 
         private static PropertyInfo PropertyName<T, TDto>(Expression<Func<TDto, T>> property)
