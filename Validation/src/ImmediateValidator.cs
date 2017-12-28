72c5db8d76bf379d148720ee1319aaf4d7540bf8
Andrey Verbin <info@studiomobile.ru>
2017-12-28 22:56:09 +0300

    Add immediate validator

---
+++ Validation/src/ImmediateValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Qoden.Validation
+{
+    public class ImmediateValidator : NonCollectingValidator
+    {
+        public static readonly ImmediateValidator Instance = new ImmediateValidator();
+
+        public override void Add(Error error)
+        {
+            throw new ErrorException(error);
+        }
+    }
+}
