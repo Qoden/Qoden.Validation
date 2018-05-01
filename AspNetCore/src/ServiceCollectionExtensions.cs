fcda0b67b61445ab3fb96bd2c872d60c12da88eb
Mikhail Khalizev <exomicky53@gmail.com>
2018-05-01 18:08:10 +0700

    Add ValidateModelAttribute class. Add AddMyMvc extensions method.

---
+++ AspNetCore/src/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Qoden.Validation.AspNetCore
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds MVC services to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add services to.</param>
+        /// <param name="setupAction">An System.Action`1 to configure the provided Microsoft.AspNetCore.Mvc.MvcOptions.</param>
+        /// <returns>An Microsoft.Extensions.DependencyInjection.IMvcBuilder that can be used to further configure the MVC services.</returns>
+        public static IMvcBuilder AddMyMvc(this IServiceCollection services, Action<MvcOptions> setupAction = null)
+        {
+            return services.AddMvc(o =>
+            {
+                o.Filters.Add<ApiExceptionFilterAttribute>();
+                o.Filters.Add<ValidateModelAttribute>();
+                setupAction?.Invoke(o);
+            });
+        }
+    }
+}
---
+++ AspNetCore/src/ValidateModelAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Qoden.Validation.AspNetCore
+{
+    /// <summary>
+    /// It checks the data model.
+    /// </summary>
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Occurs before the request is processed.
+        /// </summary>
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.IsValid)
+                return;
+
+            var errorList = context.ModelState.Values
+                .SelectMany(m => m.Errors)
+                .Select(e => string.IsNullOrEmpty(e.ErrorMessage) 
+                    ? e.Exception?.ToString() 
+                    : e.ErrorMessage)
+                .Select(x =>
+                {
+                    var err =  new Error("ModelState.IsValid", "{Msg}");
+                    err.Info["Msg"] = x;
+                    return err;
+                })
+                .ToList();
+
+            throw new MultipleErrorsException(errorList);
+        }
+    }
+}
